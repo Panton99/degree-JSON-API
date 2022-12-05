5e3c4b2aebd53133dcceae8db7bd5b5f54cfacd2
Jisoo Lee (Jisoo Kim) <98534676+Panton99@users.noreply.github.com>
2022-12-05 14:51:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,29 @@ async function getData(){ //create fetch and get respons
 	    return response.json();
     }).then( 
        response => console.log(JSON.stringify(response))
+    ).then(
+    	display(response)
     ).catch(error => {
 	    console.log(error);
     });
 }
 
+function display(data) {
+    let table = 
+        `<tr>
+          <th>School</th>
+          <th>Program/Major</th>
+          <th>Type</th>
+          <th>Year conferred</th>
+        </tr>`;
+    
+    // Loop to access all rows 
+    for (var row of data.list) {
+        table += `<tr> 
+   	 <td>${row.School} </td>
+   	 <td>${row.Progrma/Major}</td>
+   	 <td>${row.Type}</td> 
+   	 <td>${row.Yearconferred}</td>          
+</tr>`;
+    }
 
