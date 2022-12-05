bbce62a89aca24fdf5d701d57d6115656e8d3e8f
Jisoo Lee (Jisoo Kim) <98534676+Panton99@users.noreply.github.com>
2022-12-05 10:09:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ async function getData(){
     const response = await fetch('https://degree-json-api.netlify.app/degrees.json', { headers: { "Accept": "application/json" }).then(
         response => response.json()
     ).then(
-        data => statusCode: 200,
-	    
+       response => console.log(JSON.stringify(response))
     )
     .catch(error => {
         if(error == "server") {
