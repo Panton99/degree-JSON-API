b3d98b34610abc81c334864f109b1945351755be
Jisoo Lee (Jisoo Kim) <98534676+Panton99@users.noreply.github.com>
2022-12-05 11:58:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 
 
 async function getData(){
-    const response = await fetch('https://degree-json-api.netlify.app/degrees.json').then(
+    const response = await fetch('https://degree-json-api.netlify.app/degrees.json', {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers:{
+            'Content-Type': 'application/degrees.json; charset=utf-8',
+			'Access-Control-Allow-Origin': '*',
+        }
+    }).then(
         response => response.json()
     ).then(
        response => console.log(JSON.stringify(response))
