0921103181ea2ada24f16b25e1cc0709dc2f6e26
Jisoo Lee (Jisoo Kim) <98534676+Panton99@users.noreply.github.com>
2022-12-05 10:08:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
 
 
-// async function getData(){
-//     const response = await fetch('https://degree-json-api.netlify.app/degrees.json', { headers: { "Accept": "application/json" }).then(
-//         response => response.json()
-//     ).then(
-//         data => statusCode: 200,
+async function getData(){
+    const response = await fetch('https://degree-json-api.netlify.app/degrees.json', { headers: { "Accept": "application/json" }).then(
+        response => response.json()
+    ).then(
+        data => statusCode: 200,
 	    
-//     )
-//     .catch(error => {
-//         if(error == "server") {
-//             return console.log(error);
-//         }
-//     })
+    )
+    .catch(error => {
+        if(error == "server") {
+            return console.log(error);
+        }
+    })
 
-// }
-
-
-
-import fetch from "node-fetch";
-
-const API_ENDPOINT = "https://degree-json-api.netlify.app/degrees.json";
-
-exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
-    .then(response => response.json())
-    .then(data => ({
-      statusCode: 200,
-      body: data.Schools
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+}
 
 
-// document.getElementById("button").addEventListener();
-// getData().then(data => {
-//     console.log(data);
-// });
-
-// getData();
+getData();
